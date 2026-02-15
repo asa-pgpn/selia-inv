b0fd4bc48900e1fe88d124a7a390f2365d55f6c6
asa-pgpn <asa.pagupon@gmail.com>
2026-02-15 11:23:01 +0700

    message

--- app/page.jsx
+++ app/page.jsx
@@ -19,7 +19,7 @@ const ctx = getNetlifyContext();
 export default function Page() {
   return (
     <div>
-      <h1>Halo, ini halaman kosong saya!</h1>
+      <h1>Halo, ini halaman kosong saya1!</h1>
     </div>
   );
 }
