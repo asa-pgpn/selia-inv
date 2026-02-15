7b71208c8c31dcb98c39ad269efbdfa3f5a197a2
asa-pgpn <asa.pagupon@gmail.com>
2026-02-15 11:27:25 +0700

    message

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,14 @@
 import Link from 'next/link';
+import { Card } from 'components/card';
+import { ContextAlert } from 'components/context-alert';
+import { Markdown } from 'components/markdown';
+import { RandomQuote } from 'components/random-quote';
+import { getNetlifyContext } from 'utils';
+
+const contextExplainer = `
+The card below is rendered on the server based on the value of \`process.env.CONTEXT\` 
+([docs](https://docs.netlify.com/configure-builds/environment-variables/#build-metadata)):
+`;
 
 const preDynamicContentExplainer = `
 The card content below is fetched by the client-side from \`/quotes/random\` (see file \`app/quotes/random/route.js\`) with a different quote shown on each page load:
